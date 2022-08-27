0cd756cb1717755a1ebddb919ed3ca1b09b6f8f2
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-08-27 21:09:51 +0700

    fix export

--- lib/src/rx_multi_provider.dart
+++ lib/src/rx_multi_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:inherited_rx_dart/inherited_rx_dart.dart';
 import 'package:nested/nested.dart';
+import 'rx_provider.dart';
 
 class RxMultiProvider extends Nested {
   RxMultiProvider(
