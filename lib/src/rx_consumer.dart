9dddcda2711d54e4f9d3e313561e74503b99141b
SilentCat <ngocphuocnguyen312@gmail.com>
2022-08-30 14:42:59 +0700

    add doc for rx consumer

--- lib/src/rx_consumer.dart
+++ lib/src/rx_consumer.dart
@@ -5,6 +5,8 @@ import 'rx_builder.dart';
 import 'rx_bloc.dart';
 import 'type_def.dart';
 
+/// Combination of [RxListener] and [RxBuilder] for bloc [B] of type [RxBloc],
+/// state [S] and notification [N]
 class RxConsumer<B extends RxBloc<S, N>, S, N> extends StatelessWidget {
   const RxConsumer({
     Key? key,
@@ -14,9 +16,18 @@ class RxConsumer<B extends RxBloc<S, N>, S, N> extends StatelessWidget {
     this.shouldRebuildWidget,
   }) : super(key: key);
 
+  /// Callback for notification emitted from [RxBloc]
   final RxBlocEventListener<N>? notificationCallback;
+
+  /// Callback for state emitted from [RxBloc]
   final RxBlocEventListener<S>? stateCallback;
+
+  /// Typical Builder function, which called when the bloc this widget depend
+  /// on require its to rebuild itself with new value.
   final RxBlocWidgetBuilder<S> builder;
+
+  /// Function to determine whether this widget should rebuild itself when state
+  /// changed.
   final ShouldRebuildWidget<S>? shouldRebuildWidget;
 
   @override
